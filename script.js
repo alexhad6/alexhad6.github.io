1553ffcfbf04030f680e4fc512b24f1c80f2032d
alexhad6 <Alex@Alexs-Air.home>
2017-03-23 14:32:28 -0400

    update

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 let nColumns = 0;
 
 const importImg = [
-	'waterfall.jpg',
+	'blue_ridge_bonfire.jpg',
+	'central_park_waterfall.jpg',
+	'east_river_sunset.jpg',
+	'hudson_river_boats.jpg',
+	'orange_sunset.jpg',
+	'rainbow_flatiron.jpg',
 	'red_sunset.jpg',
-	'flatiron.jpg',
-	'third_avenue_night.jpg'
+	'snowy_sidewalk.jpg',
+	'southampton_beach.jpg',
+	'third_avenue_night.jpg',
+	'washington_square_arch.jpg'
 ];
 const images = [];
 for(let i = 0; i < importImg.length; i++) {
