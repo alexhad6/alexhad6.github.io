ecd812c4874c238b3c0bde0c149e33362c330152
alexhad6 <Alex@Alexs-Air.home>
2017-05-17 21:15:27 -0400

    Update

--- photo-grid.js
+++ photo-grid.js
@@ -1,12 +1,14 @@
 let nColumns = 0;
 
-function updateMain() {
-	if ($(window).width() < 640) { updateColumns(1); }
-	else if ($(window).width() < 1000) { updateColumns(2); }
-	else { updateColumns(3); }
+function updateMain(first) {
+	let nCol = 0;
+	if ($(window).width() < 640) { nCol = 1; }
+	else if ($(window).width() < 1000) { nCol = 2; }
+	else { nCol = 3; }
+	updateColumns(nCol, first);
 }
 
-function updateColumns(nCol) {
+function updateColumns(nCol, first) {
 	if (nColumns !== nCol) {
 		const columns = [];
 		$('main').empty();
@@ -18,16 +20,18 @@ function updateColumns(nCol) {
 
 		for(let i = 0; i < images.length; i++) {
 			let image = images[i];
-			images[i].object.src = images[i].src;
-			images[i].object.onload = function() { $('#img'+i).show(); };
 			columns.sort(function(c1, c2) { return c1.height - c2.height; });
 			$('#'+columns[0].id).append('<div class="container" id="img'+i+'"><a href="'+image.src+'"><img src="'+image.src+'"></a></div>');
-			$('#img'+i).hide();
 			columns[0].height += image.height/image.width;
+			if (first) {
+				$('#img'+i).hide();
+				images[i].object.src = images[i].src;
+				images[i].object.onload = function() { $('#img'+i).show(); };
+			}
 		}
 
 		nColumns = nCol;
 	}
 }
-$(document).ready(updateMain);
-$(window).resize(updateMain);
+$(document).ready(function() { updateMain(true); });
+$(window).resize(function() { updateMain(false); });
