23e8a3b4f416c661ebc0a3c8b3501c0ed367f1dd
alexhad6 <Alex@Alexs-Air.home>
2017-05-21 12:12:49 -0400

    Update

--- photo-grid.js
+++ photo-grid.js
@@ -17,25 +17,26 @@ function updateColumns(nCol) {
 		}
 
 		for(let i = 0; i < images.length; i++) {
-			const { src, width, height, object } = images[i];
-			const img = $('<div class="container"><img src="'+src+'"></div>');
+			const { src, width, height, filename, object } = images[i];
+			const img = $('<img src="'+src+'">');
 
 			columns.sort(function(c1, c2) { return c1.height - c2.height; });
 			img.appendTo(columns[0].object);
 			columns[0].height += height/width;
 
 			img.click(function() {
-				$('#grid').hide();
-				img.clone().appendTo('#active').click(function() {
-					$('#active').empty();
-					$('#grid').show();
-				});
+				img.clone().appendTo('#active');
+				$('#active').show().click(function() {
+					$('#active').empty().hide();
+					$('body').css('overflow', 'scroll');
+				});;
+				$('body').css('overflow', 'hidden');
 			});
 			
 			if (nColumns === 0) {
 				img.hide();
 				object.onload = function() { img.show(); }
-				$('#active').empty();
+				$('#active').empty().hide();
 			}
 		}
 
